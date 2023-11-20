c2582926efab620ecd1ac5dec4921664c82d3b5a
DanielCollaguazo2003 <109354364+DanielCollaguazo2003@users.noreply.github.com>
2023-11-20 08:42:19 -0500

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,12 @@ import { IndexComponent } from './pages/index/index.component';
 import { ListaTareasComponent } from './components/lista-tareas/lista-tareas.component';
 import { FormularioComponent } from './components/formulario/formulario.component';
 
+//firebase
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { FIREBASE_APP_NAME, FIREBASE_OPTIONS } from '@angular/fire/compat';
+import { environment } from '../environments/environments';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,10 +23,13 @@ import { FormularioComponent } from './components/formulario/formulario.componen
     FormularioComponent
   ],
   imports: [
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore()),
     BrowserModule,
     AppRoutingModule
   ],
   providers: [
+    { provide: FIREBASE_OPTIONS, useValue: environment.firebaseConfig },
     provideClientHydration()
   ],
   bootstrap: [AppComponent]
---
+++ src/environments/environments.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyDgyIPUworFRrS154JBWLtwKvSA79WZ-Z8",
+    authDomain: "proyecto-prueba-9bdca.firebaseapp.com",
+    projectId: "proyecto-prueba-9bdca",
+    storageBucket: "proyecto-prueba-9bdca.appspot.com",
+    messagingSenderId: "1020211746089",
+    appId: "1:1020211746089:web:9d67d97b7342ca411c9338",
+    measurementId: "G-C3G6KSRW40"
+  }
+};
